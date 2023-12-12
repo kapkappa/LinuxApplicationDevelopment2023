c98c9307a049641e115ea7dc1f1d4976670e9171
gravitage <roman.kupry@gmail.com>
2023-12-12 20:05:36 +0300

    8 HW completed

--- 08_I18n/guess.c
+++ 08_I18n/guess.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <libintl.h>
 #include <locale.h>
+#include <string.h>
+#include <ctype.h>
 
 #define _(STRING) gettext(STRING)
 
 int main(int argc, char** argv) {
 
-    bindtextdomain("guess", "po");
     setlocale(LC_ALL, "");
+    bindtextdomain("guess", ".");
     textdomain("guess");
 
     printf(_("Please, guess a number from 1 to 100.\n"));
@@ -18,13 +20,21 @@ int main(int argc, char** argv) {
         int half = (upper + lower) / 2;
         printf(_("Does the number more than %d?\n"), half);
 
-        int y;
-        scanf("%d", &y);
+        char c[8];
+        int correct_input = 0;
+        while (correct_input != 1) {
+            correct_input = scanf("%7s", c);
+            for (int i = 0; c[i] != 0; i++)
+                c[i]= tolower(c[i]);
 
-        if (y) {
-            lower = half;
-        } else {
-            upper = half;
+            if (!strcmp(c, _("yes")) || !strcmp(c, _("y")))
+                lower = half + 1;
+            else if (!strcmp(c, _("no")) || !strcmp(c, _("n")))
+                upper = half;
+            else {
+                printf(_("Bad input! Enter yes or no.\n"));
+                correct_input = 0;
+            }
         }
     }
 
--- 08_I18n/make.sh
+++ 08_I18n/make.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-
+touch NEWS README AUTHORS ChangeLog
+autoreconf -fisv
+./configure
+make
