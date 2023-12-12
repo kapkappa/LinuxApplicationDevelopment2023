8c66dc06ee46bb28264b5ee84e03050322ed8319
gravitage <roman.kupry@gmail.com>
2023-12-12 05:10:51 +0300

    first approach

--- 08_I18n/guess.c
+++ 08_I18n/guess.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
+#include <libintl.h>
+#include <locale.h>
+
+#define _(STRING) gettext(STRING)
 
 int main(int argc, char** argv) {
-    printf("Please, guess a number from 1 to 100.\n");
+
+    bindtextdomain("guess", "po");
+    setlocale(LC_ALL, "");
+    textdomain("guess");
+
+    printf(_("Please, guess a number from 1 to 100.\n"));
 
     int lower = 1, upper = 100;
 
     while (upper != lower) {
         int half = (upper + lower) / 2;
-        printf("Does the number more than %d?\n", half);
+        printf(_("Does the number more than %d?\n"), half);
 
         int y;
         scanf("%d", &y);
@@ -19,7 +28,7 @@ int main(int argc, char** argv) {
         }
     }
 
-    printf("Your number is %d!\n", lower);
+    printf(_("Your number is %d!\n"), lower);
 
     return 0;
 }
